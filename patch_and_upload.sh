bb26449cc79e4343571cb69a823d76313173d57a
Florent Lioult <florent@hihan.org>
2023-02-15 19:24:02 +0100

    Premier jet.

---
+++ patch_and_upload.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+python3 patchgame.py
+cp 2702663883.patched.json '/home/sadalsuud/.local/share/Tabletop Simulator/Saves/TS_Save_99.json'
+#cp 2702663883.png '/home/sadalsuud/.local/share/Tabletop Simulator/Saves/TS_Save_99.png'
