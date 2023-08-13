9e8225e6a91823b666676d66bbac585b09d528fe
Florent Lioult <florent@hihan.org>
2023-08-13 15:42:31 +0200

    Removing hard coded paths in scripts.

--- export.sh
+++ export.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-output_save='/home/sadalsuud/.local/share/Tabletop Simulator/Saves/TS_Save_99.json'
-tts_save_dir='/home/sadalsuud/.local/share/Tabletop Simulator/Saves'
+output_save="$HOME/.local/share/Tabletop Simulator/Saves/TS_Save_99.json"
+tts_save_dir="$HOME/.local/share/Tabletop Simulator/Saves"
 cp 'tmp/mod.patched.json' "$output_save"
 output_png_file=$(readlink -f "$output_save" | sed 's/.json/.png/g')
 if [ ! -f "$output_png_file" ]; then
