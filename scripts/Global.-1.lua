35726b55a74b76d10671f3adfe76528553255e81
Florent Lioult <florent@hihan.org>
2025-09-03 17:37:30 +0200

    Changed back the mod name.

--- scripts/Global.-1.lua
+++ scripts/Global.-1.lua
@@ -14,7 +14,7 @@
 -- Will be automatically replaced by the build timestamp.
 local BUILD = 'TBD'
 
-local MOD_NAME = 'Rakis Rising + Bloodlines'
+local MOD_NAME = 'Rakis Rising'
 
 -- Do not load anything. Appropriate to work on the mod content in TTS without
 -- interference from the scripts.
